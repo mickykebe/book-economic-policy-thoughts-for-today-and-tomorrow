2736000104c4e9b36f61c92f223c6559329dc54a
Michael K Tedla <mickykebe@gmail.com>
2021-06-25 14:02:26 +0300

    added document title

--- theme.config.js
+++ theme.config.js
@@ -23,6 +23,7 @@ export default {
   ),
   head: (
     <>
+      <title>ኢኮኖሚክ ፖሊሲ - ሀሳቦች ለዛሬ እና ለነገ</title>
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="am" />
